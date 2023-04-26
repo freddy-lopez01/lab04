e857f623a2d2faeb3092af3f8470c837307e2a8f
Freddy Lopez <freddlopez01@gmail.com>
2023-04-26 02:39:01 -0700

    first two files

--- chldhandler.c
+++ chldhandler.c
@@ -6,8 +6,50 @@
 #include <sys/wait.h>
 #define UNUSED __attribute__((unused))
 
+#define NUMBER 5
+char *args[] = {"./cpubound", "-s", "5", NULL};
+
+volatile int nprocesses = 0; 
+
+static void onusr2(UNUSED int sig){
+}
+
+static void onchld(UNUSED int sig){
+	pid_t pid; 
+	int status; 
+	printf("Number of processes --> %d\n", nprocesses);
+
+	while((pid = waitpid(-1, &status, WNOHANG)) > 0){
+		if(WIFEXITED(status) || WIFSIGNALED(status)){
+			nprocesses--; 
+			kill(getpid(), SIGUSR2);
+		}
+	}
+}
+
 
 int main(UNUSED int argc, UNUSED char *argv[]) {
+
+	int i; 
+	signal(SIGCHLD, onchld);
+	signal(SIGUSR2, onusr2);
+
+	for(i = 0; i < NUMBER; i++){
+		int pid = fork();
+		switch(pid){
+		case -1: fprintf(stderr, "Parent: fork failed\n");
+			goto wait_for_children; 
+		case 0: execvp(args[0], args);
+			fprintf(stderr, "Child: execvp() failed\n");
+			exit(EXIT_FAILURE);
+		default: nprocesses++; 
+		}
+	}
+
+	wait_for_children:
+		while(nprocesses > 0){
+			pause();
+		}
 	
 	return EXIT_SUCCESS;
 }
--- doticks.c
+++ doticks.c
@@ -8,9 +8,52 @@
 #include <stdio.h>
 
 #define UNUSED __attribute__((unused))
+#define QUANTUM 1000 /* 1000ms == 1 second */
 
+
+int ticks = 0; 
+int tick = 250; 
+
+volatile bool shutdown = false;  /* prevent the compiler from doing any optimizations */
+
+static void onalrm(UNUSED int sig){
+    if(ticks > 0){
+        printf("%d ticks until reset\n", ticks--);
+    }
+    else{
+        ticks = QUANTUM / tick;
+        printf("Resetting tick count\n");
+    }
+}
+
+static void onint(UNUSED int sig){
+    printf("^c signal received, shutting down\n");
+    return EXIT_SUCCESS; 
+}
 
 int main(int argc, char **argv) {
+
+    struct itimerval it_val; 
+    struct timespec ms20 = {0, 20000000};
+
+    if(argc == 2){
+        tick = atoi(argv[1]);
+    }
+
+    signal(SIGINT, onint);
+    signal(SIGALRM, onalrm);
+
+    it_val.it_value.tv_sec = tick / 1000; 
+    it_val.it_value.tv_usec = (tick*1000) % 1000000; 
+    it_val.it_interval = it_val.it_value;
+
+    if(setitimer(ITIMER_REAL, &it_val, NULL) == -1){
+        fprintf(stderr, "error calling setitimer()\n");
+    }
+    while(!shutdown){
+        nanosleep(&ms20, NULL);
+    }
+
 
     return EXIT_SUCCESS;
 }
