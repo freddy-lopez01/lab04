9d5b3afd164c67f626d2ee4cef1844b516ee08f9
freddy-lopez01 <122557020+freddy-lopez01@users.noreply.github.com>
2023-04-27 16:00:19 -0700

    Add files via upload

---
+++ chldhandler.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define UNUSED __attribute__((unused))
+
+
+int main(UNUSED int argc, UNUSED char *argv[]) {
+	
+	return EXIT_SUCCESS;
+}
---
+++ timeit.c
@@ -0,0 +1,12 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED __attribute__((unused))
+
+
+
+int main(UNUSED int argc, UNUSED char *argv[]) {
+
+	
+	return EXIT_SUCCESS;
+}
